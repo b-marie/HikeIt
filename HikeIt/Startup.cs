41d272d691148e6d5e13d8b2744a7e122e4f8eef
Brittany Heller <brittany.heller@live.com>
2018-04-03 12:43:00 -0600

    Add project files.

---
+++ HikeIt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HikeIt.Startup))]
+namespace HikeIt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
